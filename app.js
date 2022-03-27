490d685ba72c18fe2b7015f1ed0488d6520e599e
keefe089e <kelleybkeefer@googlemail.com>
2022-03-27 14:56:49 -0500

    completed project

--- app.js
+++ app.js
@@ -14,18 +14,29 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-  const html = `<!DOCTYPE html>
-
-<html>
-<head>
-<title> Wizard News</title>
-</head>
-<body>
-<ul>
-${posts.map((post) => `<li>${post}</li>`)}
-</ul>
-</body>
-</html>`;
+  const html =`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>
 
   res.send(html);
 });
